b8e8eb823e3f4050a5208b58840580d4162d75f5
Safa Bechchaa <safabechchaasara@gmail.com>
2024-07-08 00:36:42 +0100

    make it better

--- services/describeMetadata.js
+++ services/describeMetadata.js
@@ -1,6 +1,6 @@
 const jsforce = require('jsforce');
 
-const accessToken = '6Cel800DQy000009119l888Qy0000002POvELtyt413jJA2Hs38PmLTBfG2T1vMJMOKpW1iKq6jr4DdehJUg5ypWSxc1mfvWiBgLmIEyH7o';
+const accessToken = '6Cel800DQy000009119l888Qy0000002POvx4Lf0DDXcL2IrwZbgPxk8QisAc2SGndU0OTSuJQEqwSjkrlhaNB4C3tBa7rYLjNtH0Vd1wAZ';
 const instanceUrl = 'https://devsafa-dev-ed.develop.my.salesforce.com';
 
 const conn = new jsforce.Connection({
--- services/retrieveAllMetadataTooling.js
+++ services/retrieveAllMetadataTooling.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 // Your access token and instance URL
-const accessToken = '6Cel800DQy000009119l888Qy0000002POvELtyt413jJA2Hs38PmLTBfG2T1vMJMOKpW1iKq6jr4DdehJUg5ypWSxc1mfvWiBgLmIEyH7o';
+const accessToken = '6Cel800DQy000009119l888Qy0000002POvx4Lf0DDXcL2IrwZbgPxk8QisAc2SGndU0OTSuJQEqwSjkrlhaNB4C3tBa7rYLjNtH0Vd1wAZ';
 const instanceUrl = 'https://devsafa-dev-ed.develop.my.salesforce.com';
 
 // Create a new connection
