deacd6ae6f52961bab06a6b1b1d8553b03f75e28
Safa Bechchaa <safabechchaasara@gmail.com>
2024-07-11 11:40:16 +0100

    make it better

--- controllers/deploymentController.js
+++ controllers/deploymentController.js
@@ -14,7 +14,7 @@ exports.createDeployment = async (req, res) => {
 
     try {
         const query = `
-            INSERT INTO Deploiement (source_org_id, target_org_id, dep_name)
+            INSERT INTO deployment (source_org_id, target_org_id, dep_name)
             VALUES ($1, $2, $3)
             RETURNING *
         `;
@@ -28,15 +28,14 @@ exports.createDeployment = async (req, res) => {
     }
 };
 
-
 exports.getDeployments = async (req, res) => {
     try {
         const query = `
             SELECT d.*, o1.name as source_org_name, o2.name as target_org_name
-            FROM Deploiement d
-            JOIN Organizations o1 ON d.source_org_id = o1.org_id
-            JOIN Organizations o2 ON d.target_org_id = o2.org_id
-            ORDER BY d.created_date DESC
+            FROM deployment d
+            JOIN organizations o1 ON d.source_org_id = o1.org_id
+            JOIN organizations o2 ON d.target_org_id = o2.org_id
+            ORDER BY d.datecreated DESC
         `;
         const result = await client.query(query);
         res.status(200).json(result.rows);
