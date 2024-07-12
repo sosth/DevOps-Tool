401bf9b95d20802d233851f45ac797840c219d4e
Safa Bechchaa <safabechchaasara@gmail.com>
2024-07-12 17:35:19 +0100

    make it better

--- controllers/apexController.js
+++ controllers/apexController.js
@@ -1,13 +1,10 @@
 const retrieveService = require('../services/retrieveService');
 
 exports.retrieveApexClasses = async (req, res) => {
-    const { orgName } = req.body;
-
     try {
-        await retrieveService.retrieveApexClasses(orgName);
-        res.status(200).json({ message: 'All Apex classes retrieved and saved successfully.' });
+        await retrieveApexClasses();
+        res.status(200).send('Apex classes retrieved and written to files successfully.');
     } catch (error) {
-        console.error('Error retrieving Apex classes:', error);
-        res.status(500).json({ error: 'Failed to retrieve Apex classes' });
+        res.status(500).send('Error retrieving Apex classes: ' + error.message);
     }
 };
